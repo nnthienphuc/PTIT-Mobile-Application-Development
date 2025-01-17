6c3dc25fb47baa6cfd8f37821d9fc9696802b9c8
Nguyen Ngoc Thien Phuc <nnthienphuc@gmail.com>
2025-01-17 16:21:05 +0700

    Finish first Demo

---
+++ TestDemo/app/src/main/java/com/nnthienphuc/testdemo/MainActivity.java
@@ -0,0 +1,45 @@
+package com.nnthienphuc.testdemo;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    TextView textView;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.edtName);
+        button = findViewById(R.id.btnClickMe);
+        textView = findViewById(R.id.txtName);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fullName = editText.getText().toString();
+                textView.setText("Welcome " + fullName);
+            }
+        });
+    }
+}
